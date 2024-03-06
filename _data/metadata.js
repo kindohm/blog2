d518130af368726785b4544dbcfef813c8638109
Mike Hodnick <mike@kindohm.com>
2024-03-06 12:50:27 -0600

    random colors

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,3 +1,44 @@
+const colorable = require("colorable");
+
+const rand255 = () => {
+  return Math.floor(Math.random() * 255);
+};
+
+const randHex = () => rand255().toString(16).padStart(2, "0");
+
+const randHexColor = () => `#${randHex()}${randHex()}${randHex()}`;
+
+// console.log("colorable", JSON.stringify(result, null, 2));
+
+const getCombo = () => {
+  const randColors = new Array(10).fill(null).reduce((acc, col, i) => {
+    return { ...acc, [`color${i}`]: randHexColor() };
+  }, {});
+
+  const result = colorable(randColors, { compact: true, threshold: 0 });
+
+  const ideals = result
+    .map((c) => {
+      const match = c.combinations.find((cc) => cc.accessibility.aaa === true);
+      if (!match) return null;
+      const foreground = c.hex;
+      const background = match.hex;
+      return { foreground, background };
+    })
+    .filter((x) => !!x);
+
+  if (ideals.length === 0) {
+    console.log("random color combo not found, trying again");
+    return getCombo();
+  }
+
+  return ideals[0];
+};
+
+const { foreground, background } = getCombo();
+
+console.log("colors:", { foreground, background });
+
 module.exports = {
   title: "Mike Hodnick's Blog",
   url: "https://kindohm.com/",
@@ -15,4 +56,6 @@ module.exports = {
     "drowsiness",
     "dizziness",
   ],
+  foreground,
+  background,
 };
--- eleventy.config.js
+++ eleventy.config.js
@@ -93,7 +93,7 @@ module.exports = function (eleventyConfig) {
 
   let options = {
     html: true,
-    breaks: true,
+    breaks: false,
     linkify: true,
   };
 
