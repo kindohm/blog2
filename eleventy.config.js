c7557164ee16358ccaf1a7895b1acfef93733dc6
Mike Hodnick <mike@kindohm.com>
2024-03-06 13:31:09 -0600

    copy cname correctly

--- eleventy.config.js
+++ eleventy.config.js
@@ -16,7 +16,7 @@ module.exports = function (eleventyConfig) {
   // For example, `./public/css/` ends up in `_site/css/`
   eleventyConfig.addPassthroughCopy({
     "./public/": "/",
-    "./CNAME": "/",
+    "./CNAME": "/CNAME",
     "./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css",
   });
 
